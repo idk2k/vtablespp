ee27b20b2bab00120f327d13cf2adf5ab2bcf26f
Dmitriy S <north.projectamericas@gmail.com>
2025-07-17 14:58:20 +0700

    2nd example: class with 2 base classes and same virtual method

--- main.cpp
+++ main.cpp
@@ -1,28 +1,43 @@
 #include <iostream>
 
-class Base {
+class Base1 {
 public:
-	Base(int ogo) : ogo(ogo) {}
-	char opa{ 12 };
-	int ogo;
+	//Base(int ogo) : ogo(ogo) {}
+	int ogo{1};
 	virtual int test() = 0;
 };
 
-class Derived : public Base {
+class Base2 {
 public:
-	Derived(int ogo, int derive_ogo) : Base{ogo}, derive_ogo(derive_ogo) {}
-	int derive_ogo;
+	//Base(int ogo) : ogo(ogo) {}
+	int ogo2{ 2 };
+	virtual int test() = 0;
+};
+
+class Derived : public Base1, public Base2 {
+public:
+	//Derived(int ogo, int derive_ogo) : Base{ogo}, derive_ogo(derive_ogo) {}
+	int derived_ogo{3};
 	virtual int test() override {
 		return 8;
 	}
 };
 
+
+
 auto main() -> int {
-	Derived derived_class{ 1, 2 };
+	Derived derived_class{};
 
 	std::cout << "Derived address: " << (void*)&derived_class << std::endl;
 	
 	derived_class.test();
+	
+	Base2* bs2 = &derived_class;
+	bs2->test();
+	
+	Base1* bs1 = &derived_class;
+	bs1->test();
+
 	int a; std::cin >> a;
 	return 0;
 }
