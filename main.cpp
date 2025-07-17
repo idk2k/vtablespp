c7349e3ef65914b8a56c5e367041561862883944
Dmitriy S <north.projectamericas@gmail.com>
2025-07-17 14:31:16 +0700

    basic call test()

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 class Base {
 public:
 	Base(int ogo) : ogo(ogo) {}
+	char opa{ 12 };
 	int ogo;
 	virtual int test() = 0;
 };
@@ -20,6 +21,8 @@ auto main() -> int {
 	Derived derived_class{ 1, 2 };
 
 	std::cout << "Derived address: " << (void*)&derived_class << std::endl;
+	
+	derived_class.test();
 	int a; std::cin >> a;
 	return 0;
 }
