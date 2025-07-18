829833eb472d92ba043fc1bd3b80df5d57e9fdd3
Dmitriy S <north.projectamericas@gmail.com>
2025-07-18 10:17:47 +0700

    3rd experiment (Derived with extra virtual method)

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # vtables reference guide
 
-## MSVC
+# MSVC
 
-Here msvc implementation notes ...
+## 1. C++20 (/std:c++20)
+1. Base <-- Derived
+with "virtual test()" method;
 
-1.
-Base <-- Derived
+2. (Base1, Base2) <-- Derived
+with "virtual test()" method in both classes Base1, Base2. Override in Derived;
+
+3. (Base1, Base2) <-- Derived
+with virtual methods in Base1, Base1, and new virtual method in Derived (only in Derived meta()),
+so MSVC construct vtable of Derived as (vtable(Base1) + vtable(Derived)). Concat this two tables
+in beginning of the Derived class;
+Base2 has own offset and own vtable, with test() implementation in it, without meta() method
+from Derived class. 
+
+
--- main.cpp
+++ main.cpp
@@ -2,61 +2,43 @@
 
 class Base1 {
 public:
-	//Base(int ogo) : ogo(ogo) {}
 	int ogo{ 1 };
 	virtual int test() = 0;
 };
 
 class Base2 {
 public:
-	//Base(int ogo) : ogo(ogo) {}
 	int ogo2{ 2 };
 	virtual int test() = 0;
 };
 
 class Derived : public Base1, public Base2 {
 public:
-	//Derived(int ogo, int derive_ogo) : Base{ogo}, derive_ogo(derive_ogo) {}
 	int derived_ogo{ 3 };
 
 	virtual int test() override {
 		return 8;
 	}
 
-	virtual int meta() = 0;
-};
-
-class Gamma : public Derived {
-public:
-	virtual int meta() override {
-		return 1234;
+	virtual int meta() {
+		return 12900;
 	}
 };
 
+//class Gamma : public Derived {
+//public:
+//	virtual int meta() override {
+//		return 1234;
+//	}
+//};
 
 auto main() -> int {
-	//Derived derived_class{};
-	Gamma gamma{};
-
-	/* Gamma method meta() call */
-	gamma.meta();
-
-	Derived* der = &gamma;
-	der->meta();
-
-	//std::cout << "Derived address: " << (void*)&derived_class << std::endl;
-
-	//derived_class.test();
-
-	//Base2* bs2 = &derived_class;
-	//bs2->test();
-
-	//Base1* bs1 = &derived_class;
-	//bs1->test();
-
-	//derived_class.meta();
-
-	
+	// 
+	Derived der{};
+	Derived* p_der = &der;
+	p_der->meta();
+	der.meta();
+	der.test();
 
 	int a; std::cin >> a;
 	return 0;
